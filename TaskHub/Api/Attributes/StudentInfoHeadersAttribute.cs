6920dfe533131713fe542d2f4842da83489f4cc5
Ксения Хамитова <ksushahamitova@gmail.com>
2026-03-15 16:45:18 +0500

    Add ResponseTimeHeader, StudentInfoHeaders and ValidateUserRequest attributes

---
+++ TaskHub/Api/Attributes/StudentInfoHeadersAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Api.Attributes;
+
+public class StudentInfoHeadersAttribute : ResultFilterAttribute
+{
+    public override void OnResultExecuting(ResultExecutingContext context)
+    {
+        context.HttpContext.Response.Headers.TryAdd("X-Student-Name", "Khamitova Kseniya");
+        context.HttpContext.Response.Headers.TryAdd("X-Student-Group", "RI-240932");
+    }
+}
---
+++ TaskHub/Api/Attributes/ValidateUserRequestAttribute.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Reflection;
+
+namespace Api.Attributes;
+
+public class ValidateUserRequestAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        var parameter = context.ActionArguments.Values.FirstOrDefault();
+
+        if (parameter == null)
+        {
+            context.Result = new BadRequestObjectResult("Тело запроса отсутствует");
+            return;
+        }
+
+        var nameProperty = parameter.GetType().GetProperty("Name", BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+
+        if (nameProperty == null)
+        {
+            base.OnActionExecuting(context);
+            return;
+        }
+
+        var nameValue = nameProperty.GetValue(parameter) as string;
+
+        if (string.IsNullOrWhiteSpace(nameValue))
+        {
+            context.Result = new BadRequestObjectResult("Имя пользователя не задано");
+            return;
+        }
+    }
+}
